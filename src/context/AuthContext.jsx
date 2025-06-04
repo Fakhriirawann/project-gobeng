ada776b00d7ef1e21fd6b6682cf5abe9d5fa426c
Fakhriirawann <youranxietyyy03@gmail.com>
2025-06-04 13:36:16 +0700

    fix import

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -10,7 +10,7 @@ import {
   doc,
   updateDoc,
 } from "firebase/firestore";
-import { db } from "../firebase"; // pastikan file firebase.js menginisialisasi Firestore
+import { db } from "../FireBase";
 
 export const AuthContext = createContext();
 
